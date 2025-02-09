a92b1a795780820171fa9894630264f7d29f746d
jjdlcd <jjdelacalledguez@gmail.com>
2025-02-09 19:56:25 +0100

    Cambios del tutorial parte 4

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# import dj_database_url
+
+# db_from_env = dj_database_url.config(default='postgres://alumnodb:alumnodb@localhost:5432/psi', conn_max_age=500)
+
+# DATABASES['default'].update(db_from_env)
