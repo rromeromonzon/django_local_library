1685f8cca50b30eab02318136e67d9da6b9bd728
jjdlcd <jjdelacalledguez@gmail.com>
2025-02-18 20:26:01 +0100

    arreglar ALLOWED_HOSTS

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', ('True')) == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onreder.com']
 
 
 # Application definition
